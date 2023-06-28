b3ebd6b94e310486ae0c05a44037842ba3340aab
César Guerra <warderer@gmail.com>
2023-06-27 23:13:14 -0600

    ✨ Header con NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,29 @@
+import { NavLink } from 'react-router-dom'
+
 const Header = () => {
   // Simple Header de: https://getbootstrap.com/docs/5.0/examples/headers/
   return (
     <div className='header-container'>
       <div className='container'>
         <header className='d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom'>
-          <a href='/' className='d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none'>
+          <NavLink to='/' className='d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none'>
             <svg className='bi me-2' width='40' height='32'><use xlinkHref='#bootstrap' /></svg>
             <span className='fs-4'>Piquito Dorado</span>
-          </a>
+          </NavLink>
 
           <ul className='nav nav-pills'>
-            <li className='nav-item'><a href='/' className='nav-link active' aria-current='page'>Inicio</a></li>
-            <li className='nav-item'><a href='/about' className='nav-link text-dark'>Acerca de</a></li>
-            <li className='nav-item'><a href='/faq' className='nav-link text-dark'>Preguntas Frecuentes</a></li>
-            <li className='nav-item'><a href='/contact' className='nav-link text-dark'>Contacto</a></li>
+            <li className='nav-item'>
+              <NavLink to='/' className='nav-link active' aria-current='page'>Inicio</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink to='/about' className='nav-link text-dark'>Acerca de</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink to='/faq' className='nav-link text-dark'>Preguntas Frecuentes</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink to='/contact' className='nav-link text-dark'>Contacto</NavLink>
+            </li>
           </ul>
         </header>
       </div>
