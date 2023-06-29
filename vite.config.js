d62bee524a8f442b3bfc3531fd61812b49f8f88a
César Guerra <warderer@gmail.com>
2023-06-29 10:39:28 -0600

    Vite config for local and deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/piquito-de-oro/',
-  plugins: [react()],
+export default defineConfig(({command}) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/piquito-de-oro/'
+  }
+
+  return config
 })
